a046d69e22153fbf8d7fb15d7972f1ec6901333c
Max Hacking <max@hacking.co.uk>
2020-02-08 01:23:56 +0000

    Improve grammar in PR description.

--- entrypoint.sh
+++ entrypoint.sh
@@ -115,7 +115,7 @@ if git_commit "Automated update of ${ebuild_cat}/${ebuild_pkg} keywords"; then
 	# Create a pull request
 	if [[ -n "${INPUT_AUTH_TOKEN}" ]]; then
 		title="Automated update of ${ebuild_cat}/${ebuild_pkg}"
-		msg="Automatically generated pull request to update overlay for new keywords for ${ebuild_cat}/${ebuild_pkg}"
+		msg="Automatically generated pull request to update overlay with new keywords for ${ebuild_cat}/${ebuild_pkg}"
 		create_pull_request "${overlay_branch}" "master" "${title}" "${msg}" "false" 
 	fi
 fi
